b94f4b5c3a36611acd9b6dc39456c53f5c739fe7
ujjwalparsai <ujjwalparsai109@gmail.com>
2023-09-25 19:52:56 +0530

    new code

--- src/componets/chat/chat.js
+++ src/componets/chat/chat.js
@@ -1,17 +1,36 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import './chat.css'
+import "./chat.css"
 import { Link } from 'react-router-dom';
+
 const Chat = () => {
     const [currentUsers, setCurrentUsers] = useState([]);
-    const [textmassage, setmassage] = useState([]);
-    const [receiver, setReceiver] = useState('650d68c2e769bdf99c32b797');
+    const [textmessage, setTextMessage] = useState('');
+    const [receiver, setReceiver] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [selectedReceiverName, setSelectedReceiverName] = useState('');
+    const [filteredMessages, setFilteredMessages] = useState([]);
 
     const userData = JSON.parse(localStorage.getItem('user'));
     const sender = userData._id;
+    let CurrentUser = userData.firstName;
 
-    let CurrentUser = userData.firstName
+    const selectReceiver = async (userId, userName) => {
+        setReceiver(userId);
+        setSelectedReceiverName(userName);
 
+        try {
+            const response = await axios.get("http://localhost:3000/chat/getMessages", {
+                params:
+                {
+                    receiverId: userId, senderId: sender,
+                },
+            });
+            setMessages(response.data.messages);
+        } catch (err) {
+            console.log(err);
+        }
+    };
     useEffect(() => {
         async function fetchData() {
             try {
@@ -23,42 +42,84 @@ const Chat = () => {
         }
         fetchData();
     }, []);
+
+    useEffect(() => {
+        const filtered = messages.filter((message) => {
+            return (message.sender === sender && message.receiver === receiver) ||
+                (message.sender === receiver && message.receiver === sender);
+        });
+        setFilteredMessages(filtered);
+    }, [messages, sender, receiver]);
+
     const sendMassage = async (event) => {
         event.preventDefault();
         try {
+            if (textmessage.trim() === '') {
+                console.log('Please enter a message.');
+                return;
+            }
             const response = await axios.post("http://localhost:3000/chat/send", {
-                textmassage: textmassage,
+                textmassage: textmessage,
                 sender: sender,
                 receiver: receiver
             });
-            console.log(response)
+            console.log(response);
+            const newMessage = {
+                text: textmessage,
+                sender: sender,
+                receiver: receiver,
+            };
+            setMessages([...messages, newMessage]);
+            setTextMessage('');
         } catch (err) {
             console.log(err);
         }
     }
     return (
-        <div className='maindiv'>
-            <div className='innerDiv'>
-                <h3 className='text'>welcome @ "{CurrentUser}"</h3>
-                <h2><hr className='text'></hr></h2>
-                <h4>
-                    <span className='chathedding'>Chat Box</span>
-                    <hr className='text'></hr>
-                    <ul>
-                        {currentUsers.map((user, index) => (
-                            <li className='text username' key={index}>{user.firstName} {user.lastName}</li>
-                        ))}
-                    </ul>
-                    <button type="submit" class="btn btn-primary mb-2 "> <Link to="/"></Link>sign out</button>
-      
-                </h4>
-            </div>
-            <div className='chatView'>
-
-                <input type="text" className="form-control textfiled" onChange={(e) => setmassage(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your massage" />
-                <input type="hidden" className="form-control textfiled" onChange={(e) => setReceiver(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp" />
-
-                <button type="submit" onClick={sendMassage} class="btn btn-primary mb-2 sendbutton">Send</button>
+        <div className='container-fluid mt-5'>
+            <div className='row'>
+                <div className='col-md-4 line'>
+                    <div className='chat-view'>
+                        <div className='innerDiv'>
+                            <h3 className='text'>Welcome, {CurrentUser}!</h3>
+                            <hr></hr>
+                            <br></br>
+                            <hr className='text' />
+                            <h4 className='chathedding'>Chat Box</h4>
+                            <ul className='list-group'>
+                                {currentUsers.map((user, index) => (
+                                    <li
+                                        className={`list-group-item ${user._id === receiver ? 'active' : ''}`}
+                                        key={index}
+                                        onClick={() => selectReceiver(user._id, `${user.firstName} ${user.lastName}`)}>
+                                        {user.firstName} {user.lastName}
+                                    </li>
+                                ))}
+                            </ul>
+                            <Link to="/" className="btn btn-primary mt-3">Sign Out</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-8'>
+                    <h2>{selectedReceiverName}</h2>
+                    <hr></hr>
+                    <div className='chat-box'>
+                        <div className="message-display">
+                            {filteredMessages.map((message, index) => (
+                                <div key={index} className="message">
+                                    <strong>{message.sender === sender ? 'You' : selectedReceiverName} : </strong>{message.textmassage}{message.text}
+                                </div>
+                            ))}
+                        </div>
+                        <div className="input-group lastDiv ">
+                            <input type="text" className="form-control" value={textmessage} onChange={(e) => setTextMessage(e.target.value)} placeholder="Enter your message"
+                            />
+                            <div className="input-group-append">
+                                <button type="submit" onClick={sendMassage} className="btn btn-outline-primary"> Send</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
